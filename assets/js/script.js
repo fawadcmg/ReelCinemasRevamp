b1f8987758cf192273c36366a729e5a30f7386b9
umairdeziner <umairahmed.ui@gmail.com>
2018-11-28 03:57:50 +0500

    new page and old page edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3213,3 +3213,63 @@ $(document).ready(function (e) {
 		}, 'xml');
 	});
 });
+
+$('.date-items').slick({
+	dots: false,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 7,
+	slidesToScroll: 1,
+	responsive: [{
+		breakpoint: 1024,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: true,
+			dots: true
+		}
+	}, {
+		breakpoint: 600,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+	}, {
+		breakpoint: 480,
+		settings: {
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			arrows: false
+		}
+	}]
+});
+
+$('.time-itemss').slick({
+	dots: false,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	responsive: [{
+		breakpoint: 1024,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: true,
+			dots: true
+		}
+	}, {
+		breakpoint: 600,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+	}, {
+		breakpoint: 480,
+		settings: {
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			arrows: false
+		}
+	}]
+});
