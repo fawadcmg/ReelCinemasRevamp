fc203f298eb7cfc243a8057fa5d4924571c5b95d
Imran Bughio <37036873+ImranBugCMG@users.noreply.github.com>
2018-12-17 18:11:36 +0400

    wip

--- src/json-movie-detail.js
+++ src/json-movie-detail.js
@@ -211,7 +211,7 @@ function loadMovieDetail(movieName){
 		                strLen = tempMovieLanguage[counter].length;
 			  			strposition = tempMovieLanguage[counter].indexOf('Subtitle: ');
 			  			tempSubtile = tempMovieLanguage[counter].substring(strposition+10,strLen);
-			  			movieSubtitle =  '<i class="icon medium"><img src="assets/img/icons/subtitles.svg" alt="FB" class=""></i><span>'+tempSubtile+'</span>'			  			
+			  			movieSubtitle =  '<i class="icon medium"><img src="assets/img/icons/subtitles-white.svg" alt="FB" class=""></i><span>'+tempSubtile+'</span>'			  			
 		            }
 			 	}
 			
