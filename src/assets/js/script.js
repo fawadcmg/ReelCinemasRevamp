79d0a2593ae25a347952e27483c7492896bc1557
Imran Bughio <imran_bug81@hotmail.com>
2019-02-28 14:22:25 +0500

    wip

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -10305,8 +10305,10 @@ var isRTL = $('html').attr('dir') == "rtl" ? true : false,
     bodyTopPos,
     isIE = detectIE(),
     scrollHeight = 0,
+    isMobil,
     movieListAnimating = false;
 
+mobilecheck();
 // loadPlayMovies();
 charLimitInit();
 ChangeToSvg();
@@ -10315,10 +10317,7 @@ initSlick();
 headerSpace();
 adjustContentList2();
 onlyPortrait();
-
-if (winWidth < 768) {
-	$('.js-movie-list .u-loader').remove();
-}
+removeLoaderInMob();
 
 $(function () {
 	locMapInit();
@@ -10349,6 +10348,7 @@ $(function () {
 	calcScrollHeightDOM();
 	heightMediaQuery();
 	adjustContentList2();
+	navDropDownHeight();
 
 	$('.c-loader').fadeOut('slow', function () {
 		if (winWidth > 1024) {
@@ -10372,12 +10372,14 @@ $(window).on('load', function () {
 		calcScrollHeightDOM();
 		heightMediaQuery();
 	}, 200);
+	navDropDownHeight();
 });
 
 //On Window Resize
 var resizeTimer;
 $(window).on('resize orientationchange', function () {
 	if (winWidth != viewport().width) {
+		mobilecheck();
 		winDimensions();
 		heightMediaQuery();
 		setOnTopClass();
@@ -10386,21 +10388,26 @@ $(window).on('resize orientationchange', function () {
 		animWrapHeight();
 		bgMobImg();
 		adjustContentList2();
+		removeLoaderInMob();
 
 		clearTimeout(resizeTimer);
 		resizeTimer = setTimeout(function () {
 			winDimensions();
+			animWrapHeight();
+			heightMediaQuery();
 			movieListSetHTML();
 			calcScrollHeightDOM();
+			onlyPortrait();
 		}, 250);
 		movieListCarousel();
 		calcScrollHeightDOM();
 		onlyPortrait();
+		navDropDownHeight();
 	}
 });
 
 function onlyPortrait() {
-	if (winWidth < 768 && winWidth > winHeight && isMobile) {
+	if (winWidth > winHeight && isMobile) {
 		$('.c-landscape-msg').show();
 	} else {
 		$('.c-landscape-msg').hide();
@@ -10604,6 +10611,7 @@ function initSlick() {
 		slidesToScroll: 1,
 		swipeToSlide: true,
 		touchThreshold: 7,
+		speed: 600,
 		infinite: false,
 		asNavFor: '.js-main-carousel',
 		focusOnSelect: true,
@@ -12433,6 +12441,8 @@ function heightMediaQuery() {
 		if (winHeight <= 520) {
 			$('.c-movies-list .movie-lsit-wrap .movie-item .img').css('width', winHeight * 0.24);
 		}
+	} else {
+		$('.c-main-banner .main-carousel .item .item-inner').css('height', '');
 	}
 
 	if (winWidth >= 1025) {
@@ -12565,11 +12575,29 @@ function calcNumberOfLines(target) {
 	return numberOfLines;
 }
 
-window.mobilecheck = function () {
+function mobilecheck() {
 	var check = false;
 	(function (a) {
 		if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
 	})(navigator.userAgent || navigator.vendor || window.opera);
-	return check;
-};
-var isMobile = mobilecheck();
+	isMobile = check;
+}
+
+var navDropDownHeightFirstRun = false;
+function navDropDownHeight() {
+	bodyHeight = winHeight - headerHeight - 40;
+	$('.c-main-header .header-links > ul > li > ul').css('maxHeight', bodyHeight);
+	if (navDropDownHeightFirstRun == false && winWidth > 1024) {
+		navDropDownHeightFirstRun = true;
+		$('.c-main-header .header-links > ul > li > ul').mCustomScrollbar({
+			axis: "y",
+			theme: "minimal-dark"
+		});
+	}
+}
+
+function removeLoaderInMob() {
+	if (winWidth < 768) {
+		$('.js-movie-list .u-loader').remove();
+	}
+}
